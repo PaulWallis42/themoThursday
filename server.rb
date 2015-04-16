67e10abc1c2610678f880a7a8cdc8c72240859a8
paulwallis42 <paulwallis42@gmail.com>
2015-04-16 12:10:01 +0100

    working

--- server.rb
+++ server.rb
@@ -1,3 +1,15 @@
 require 'sinatra'
+enable :sessions
 
 set :public_folder, proc { File.join(root) }
+
+get ('/therm_disp') do
+
+"
+#{session[:temp]}
+"
+end
+
+post ('/therm_disp') do
+session[:temp] = params.keys.last
+end
--- src/displayingThermostat.js
+++ src/displayingThermostat.js
@@ -2,7 +2,18 @@
 
  var thermostat = new Thermostat();
 
- var updateColour = function () {
+ var thermDisp = function() {
+  //  $('<form action="/therm_disp" method="post"><input type="hidden" name="therm"></form>').submit();
+  $.ajax({
+    type: "POST",
+    url: '/therm_disp',
+    data: thermostat.temperature().toString()
+  });
+
+
+ };
+
+ var updateColour = function() {
    if (thermostat.colour() === 'green' ) {
       $("#temperature").css({'color' : 'rgb(0, 255, 0)', 'font-size' : '200%'});
   } else if (thermostat.colour() === 'yellow') {
@@ -20,18 +31,21 @@
     thermostat.up();
     updateColour();
     $("#temperature").text(thermostat.temperature());
+    thermDisp();
   });
 
   $("#down").click(function() {
     thermostat.down();
     updateColour();
     $("#temperature").text(thermostat.temperature());
+    thermDisp();
   });
 
   $("#reset").click(function() {
      thermostat.resetTemp();
      updateColour();
     $("#temperature").text(thermostat.temperature());
+    thermDisp();
    });
 
    $('#saverOff').click(function() {
